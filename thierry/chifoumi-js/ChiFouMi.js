87c7a824ceba2da692ff4e7e60747122e63f2608
thierry-a <thierryarzal353@gmail.com>
2019-03-01 15:06:51 +0400

    chifumi first part

---
+++ thierry/chifoumi-js/ChiFouMi.js
@@ -0,0 +1,13 @@
+    
+var votreChoix = prompt('Quel est votre choix: pierre, feuille, ciseaux?');
+alert('Vous avez choisi: ' + votreChoix + ' ! :-)');
+var ordiChoix = 'Pierre';
+alert('Ordi a choisi: ' + ordiChoix + ' ! :-)');
+
+	/***********     
+ciseaux > feuille (les ciseaux coupent la feuille)
+pierre > ciseaux (la pierre casse les ciseaux)
+feuille > pierre (la feuille enveloppe la pierre)
+
+ciseaux = 
+*************/
