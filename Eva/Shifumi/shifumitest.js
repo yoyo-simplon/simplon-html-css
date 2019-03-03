f3a32c5fd379ce55287d45eb101b438406342d3f
HivyLink <h.minatchy@gmail.com>
2019-03-03 17:02:37 +0000

    add shifumi

---
+++ Eva/Shifumi/shifumitest.js
@@ -0,0 +1,28 @@
+var choixUtilisateur= prompt("choisissez Pierre, feuille ou ciseaux!")
+var choix = ["pierre", "feuille", "ciseaux"]
+var aleatoire = (Math.floor(Math.random() * 2));
+
+
+
+ordinateur_choix = choix[aleatoire];
+
+
+
+
+
+if (choixUtilisateur== ordinateur_choix) {
+    alert("Match null");
+
+
+} else if ((choixUtilisateur == "pierre" && ordinateur_choix == "ciseau") || (choixUtilisateur == "feuille" && ordinateur_choix =="pierre") || (choixUtilisateur== "ciseau" && ordinateur_choix =="feuille" )) {
+    alert("L'oridinateur à choisi " + ordinateur_choix + " ...You Win ! ");
+
+
+
+} else {
+
+    alert("L'oridinateur à choisi " + ordinateur_choix + " ... You loose try again ! ");
+}
+
+console.log("Mon choix : " + choixUtilisateur);
+console.log("Celui de l'ordi : " + ordinateur_choix);
