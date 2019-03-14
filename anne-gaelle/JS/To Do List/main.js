fb75076fb3927d4504e957ca3e060b7bd754e7e6
Shi974 <shidl@rocketmail.com>
2019-03-14 15:14:46 +0400

    todolist final version

--- anne-gaelle/JS/To Do List/main.js
+++ anne-gaelle/JS/To Do List/main.js
@@ -1,7 +1,6 @@
 var app = new Vue({
     el: '#app',
     data: {
-        editedTodo: null,
         todos: [{
                 text: 'Learn JavaScript',
                 completed: true,
@@ -16,22 +15,23 @@ var app = new Vue({
                 text: 'Build something awesome',
                 completed: false,
                 edit: false,
+            },
+            {
+                text: 'Play GTA Online',
+                completed: true,
+                edit: false,
             }
         ]
     },
     methods: {
         addTodo: function (e) { // Ajoute dynamiquement à l'array donc à la liste
             this.todos.push({
-                text: e
+                text: e, completed: false, edit: false
             });
             this.element = ''; // Clear form après envoi
         },
         removeTodo: function (e) { // Fonction pour enlever un item
             this.todos.splice(this.todos.indexOf(e), 1);
-        },
-        editTodo: function (todo) { // Edition du nom de la tâche au double click
-            this.editedTodo = todo;
-            //TODO// BUG : ne fonctionne que sur les items déjà existants dans l'array ou alors en cochant la checkbox => problème de dynamisme ?!
         },
     },
     computed: {
