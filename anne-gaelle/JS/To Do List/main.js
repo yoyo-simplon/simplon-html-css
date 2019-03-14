12771a6252b0db3df87d290e76abff300e416fad
Shi974 <shidl@rocketmail.com>
2019-03-14 11:56:51 +0400

    update todo list project

--- anne-gaelle/JS/To Do List/main.js
+++ anne-gaelle/JS/To Do List/main.js
@@ -1,31 +1,42 @@
 var app = new Vue({
     el: '#app',
     data: {
+        editedTodo: null,
         todos: [{
                 text: 'Learn JavaScript',
-                completed: true
+                completed: true,
+                edit: false,
             },
             {
                 text: 'Learn Vue',
-                completed: false
+                completed: false,
+                edit: false,
             },
             {
                 text: 'Build something awesome',
-                completed: false
+                completed: false,
+                edit: false,
             }
         ]
     },
     methods: {
         addTodo: function (e) { // Ajoute dynamiquement à l'array donc à la liste
-            this.todos.push({text: e});
-            e.resetForm(); // sensé faire un clear form après l'ajout à l'array - ne fonctionne pas :<
+            this.todos.push({
+                text: e
+            });
+            this.element = ''; // Clear form après envoi
         },
-        removeTodo: function (e) { // FONCTION POUR ENLEVER UN ITEM ... JE DOIS TROUVER COMMENT LIER L'INDEX
-            // this.todos.splice({text: e}); // NOT GOOD - enlève TOUTE l'array, need index - //TODO// le remove
-            //this.todos.splice(this.todos.indexOf(e),1); // Méthode actuelle : remove par le bas
-            this.todos.splice(e,1); // Remove par le haut ....
+        removeTodo: function (e) { // Fonction pour enlever un item
+            this.todos.splice(this.todos.indexOf(e), 1);
+        },
+        editTodo: function (todo) { // Edition du nom de la tâche au double click
+            this.editedTodo = todo;
+            //TODO// BUG : ne fonctionne que sur les items déjà existants dans l'array ou alors en cochant la checkbox => problème de dynamisme ?!
+        },
+    },
+    computed: {
+        count() { // Renvoie un compteur de tâches non complétées
+            return this.todos.filter(todo => !todo.completed).length;
         }
-    }
+    },
 });
-
-//TODO// A FAIRE : clear this.form
