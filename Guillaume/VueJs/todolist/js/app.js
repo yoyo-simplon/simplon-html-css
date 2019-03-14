2b43d6c7fe0ae5ab00046808543e1b042ca73bdf
Guillaume97490 <techer.guillaume97490@gmail.com>
2019-03-14 22:15:40 +0400

    todolist : add checkbox

--- Guillaume/VueJs/todolist/js/app.js
+++ Guillaume/VueJs/todolist/js/app.js
@@ -1,38 +1,23 @@
-
-
 new Vue({
-    el: '#app',
-    data: {
-        // newTache: 'hello',
-      taches: [{value: '' , test:'', termine: false}
-          
-      ],
-    //   success: false,
+  el: '#app',
+  data: {
+    taches: [
+
+    ],
+  },
+  methods: {
+    addTache: function () {
+      this.taches.push({
+        value: '',
+        test: '',
+        termine: false
+      });
     },
-    methods: {
-      addTache: function () {
-        this.taches.push({ value: '' , test:'' , termine: false});
-      },
-      deleteTache: function (index) {
-        console.log(index);
-        console.log(this.taches);
-        this.taches.splice(index, 1);
-      },
+    deleteTache: function (index) {
+      console.log(index);
+      console.log(this.taches);
+      this.taches.splice(index, 1);
+    },
 
-      
-      // termineTache: function (index) {
-      //   console.log(index);
-      //   this.taches.push({index, termine: 'toto'});
-      //   console.log(this.taches);
-
-      // },
-
-      termineTache: function (tache){
-        // console.log(index);
-        tache.termine = true;
-        console.log(tache)
-      }
-      
-      
-    }
-  });
+  }
+});
