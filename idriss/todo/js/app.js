ccf2880adb09628a9013db254404e38c2430dae0
Idriss <fannio.idriss@gmail.com>
2019-03-28 18:19:56 +0400

    update

---
+++ idriss/todo/js/app.js
@@ -0,0 +1,26 @@
+new Vue({
+  el: "#app",
+  data: {
+    title: "hello",
+    newTodo:"",
+    todos: []
+  },
+  methods: {
+    addTodo() {
+      this.todos.push({
+        title: this.newTodo,
+        done: false
+      });
+      this.newTodo="";
+    },
+    removeTodo(todo){
+      const todoIndex= this.todos.indexOf(todo);
+      this.todos.splice(todoIndex,1);
+    },
+    allDone(){
+      this.todos.forEach(todo => {
+        todo.done = true;
+      });
+    }
+  }
+});
