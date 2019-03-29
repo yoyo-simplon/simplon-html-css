c940c4bb7f7247567cac475bfb3e15b3cc7e1f5f
Micka7 <m.pontiac07@gmail.com>
2019-03-29 11:08:16 +0400

    tchat etc...

---
+++ micka/tchat2/public/client.js
@@ -0,0 +1,32 @@
+var socket = io();
+
+$('form').submit(function(e) {
+    e.preventDefault();
+    var message = {
+        text : $('#m').val()
+    }
+
+    socket.emit('chat-message', message);
+    $('#m').val('');
+    return false;
+    if (message.text.trim().length !== 0) {
+        socket.emit('chat-message', message);
+    }
+    $('#chat input').focus();
+});
+
+socket.on('chat-message', function (message) {
+    $('#messages').append($('<li>').text(message.text));
+  });
+
+  $('#login form').submit(function (e) {
+    e.preventDefault();
+    var user = {
+      username : $('#login input').val().trim()
+    };
+    if (user.username.length > 0) { // Si le champ de connexion n'est pas vide
+      socket.emit('user-login', user);
+      $('body').removeAttr('id'); // Cache formulaire de connexion
+      $('#chat input').focus(); // Focus sur le champ du message
+    }
+  });
