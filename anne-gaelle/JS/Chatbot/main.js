034224b5fd31eb2271e6c3d98779b8539d1ec6e1
Shi974 <shidl@rocketmail.com>
2019-03-29 11:39:59 +0400

    add channels to chat box

--- anne-gaelle/JS/Chatbot/main.js
+++ anne-gaelle/JS/Chatbot/main.js
@@ -4,6 +4,7 @@ document.getElementById("name").innerHTML = prenom;
 new Vue({
     el: '#app',
     data: {
+        selected: 'Bot-spam',
         botMessage: '',
         youMessage: '',
         messages: [{
@@ -11,49 +12,72 @@ new Vue({
                 author: 'bot',
                 edit: false,
                 timestamp: moment().format('LTS'),
+                selected: 'Bot-spam',
             },
             {
                 body: 'Merci Bot!',
                 author: 'you',
                 edit: false,
                 timestamp: moment().format('LTS'),
+                selected: 'Bot-spam',
             },
             {
                 body: "Fais comme chez toi!",
                 author: 'bot',
                 edit: false,
                 timestamp: moment().format('LTS'),
+                selected: 'Bot-spam',
+            },
+            {
+                body: "Let's play a game...",
+                author: 'bot',
+                edit: false,
+                timestamp: moment().format('LTS'),
+                selected: 'Gaming',
+            },
+            {
+                body: "Work hard, bot hard!",
+                author: 'bot',
+                edit: false,
+                timestamp: moment().format('LTS'),
+                selected: 'Work',
             }
         ]
     },
     methods: {
         sendMessage: function (e) { // Ajoute dynamiquement à l'array donc à la liste
-        if (e === 'you') {
-            this.messages.push({
-                body: this.youMessage,
-                author: 'you',
-                timestamp: moment().format('LTS'),
-                edit: false,
-            }),
-            this.youMessage = ''; // Clear form après envoi
-        } else {
-            this.messages.push({
-                body: this.botMessage,
-                author: 'bot',
-                timestamp: moment().format('LTS'),
-                edit: false,
-            }),
-            this.botMessage = ''; // Clear form après envoi
-        }
+            if (e === 'you') {
+                this.messages.push({
+                        body: this.youMessage,
+                        author: 'you',
+                        timestamp: moment().format('LTS'),
+                        edit: false,
+                        selected: this.selected,
+                    }),
+                    this.youMessage = ''; // Clear form après envoi
+            } else {
+                this.messages.push({
+                        body: this.botMessage,
+                        author: 'bot',
+                        timestamp: moment().format('LTS'),
+                        edit: false,
+                        selected: this.selected,
+                    }),
+                    this.botMessage = ''; // Clear form après envoi
+            }
         },
         clearAllMessages() {
             this.messages = []
         },
-        removeMessage: function (e) { // Fonction pour enlever un item
-        this.messages.splice(this.messages.indexOf(e), 1);
-    }
+        removeMessage: function (e) { // Enlève un item
+            this.messages.splice(this.messages.indexOf(e), 1);
+        },
+        channelSelected: function (messages, selected) { // Retourne les messages en fonction des salons
+            return messages.filter(function (f) {
+                return f.selected == selected;
+            })
+        }
     },
-   
 });
 
-// https://codepen.io/manifoldkaizen/pen/oqzOKw
+//TODO// Ajouter la fonction pour mettre en stockage local et rappeler
